a376b36f3a6fc1a142227e7327642dfc64517cd0
John Soatra <johnsoatra@gmail.com>
2021-10-11 17:21:29 +0700

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { makeStyles, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
+import { blue, green } from '@material-ui/core/colors';
 import React from 'react';
 import logo from "../img/logo.jpg";
 
 const styles = makeStyles(theme => ({
   name: {
-    flex: 1,
-    paddingLeft: 20,
-    letterSpacing: 0.7
+    letterSpacing: 0.2,
+    paddingLeft: 5,
+    [theme.breakpoints.down("xs")]: {
+      display: "none"
+    }
   },
   logo: {
     borderRadius: 20
@@ -18,8 +20,21 @@ const styles = makeStyles(theme => ({
   toolbar: {
     backgroundColor: blue[900],
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-between",
     minHeight: 50
+  },
+  brand: {
+    display: "flex",
+    alignItems: "center"
+  },
+  button: {
+    color: blue[100],
+    borderColor: blue[100],
+    [theme.breakpoints.down("xs")]: {
+      padding: 2,
+      fontSize: 12,
+      margin: 3
+    }
   }
 }));
 
@@ -28,13 +43,22 @@ const Header = props => {
   return (
   <AppBar position="sticky">
     <Toolbar className={classes.toolbar}>
-      <div>
+      <div className={classes.brand}>
         <img src={logo} alt="logo" width={50} className={classes.logo} />
-      </div>
-      <div>
-        <Typography className={classes.name} variant='h5'>
+        <Typography className={classes.name}>
           Blue Standard Water Tech
         </Typography>
+      </div>
+      <div>
+        <Button className={classes.button} variant='outlined'size='small'>
+          Documents
+        </Button>
+        <Button className={classes.button} variant='outlined' size='small'>
+          Products
+        </Button>
+        <Button className={classes.button} variant='outlined'size='small'>
+          Service
+        </Button>
       </div>
     </Toolbar>
   </AppBar>
