f0c653a227b54a84854413e18ebc55d2551dc692
John Soatra <johnsoatra@gmail.com>
2021-10-16 12:28:08 +0700

    ok

--- index.js
+++ index.js
@@ -7,31 +7,8 @@ const port = process.env.port || 8080;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.send({msg: "Home page"});
 });
-
-app.get("/**", (req, res) => {
-  res.send({msg: "Not found"});
-});
-// app.get("/:appName/**", (req, res) => {
-//   let url = req.url;
-//   let appName = req.params.appName;
-//   if (url === `/${appName}/`) { 
-//     url = `/${appName}/index.html`;
-//   }
-//   res.sendFile(path.join(__dirname, url));
-// });
-
-// app.get("/:appName", async (req, res) => {
-//     const fullUrl = `https://${req.hostname}${req.url}/index.html`;
-//     axios.get(fullUrl).then(
-//       value => {
-//         res.send(value.data);
-//       }, reason => {
-//         res.send(reason);
-//     });
-// })
-
 
 app.listen(port, () => console.log("App is running!"));
