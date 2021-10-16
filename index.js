31eeb1556c403a0feb61f6d6242bb3c3b303beae
John Soatra <johnsoatra@gmail.com>
2021-10-16 11:28:13 +0700

    abit

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const storage = firebase.storage();
 const buck = storage.bucket("gs://blue-standard.appspot.com");
 app.use(cors());
 app.get("/*", async (request, response) => {
-  const path = request.path.replace("/", "");
-  await buck.file(path).get().then(success =>
-    success[0].download().then(data =>
-      response.end(data[0])
-    , err => response.json({msg: "error", content: err}))
-  , err =>
-    response.json({msg: "error", content: err})
-  );
+  // const path = request.path.replace("/", "");
+  // await buck.file(path).get().then(success =>
+  //   success[0].download().then(data =>
+  //     response.end(data[0])
+  //   , err => response.json({msg: "error", content: err}))
+  // , err =>
+  //   response.json({msg: "error", content: err})
+  // );
+  response.send("Hello");
 });
 
 app.listen(port, () => console.log("App is running!"));
