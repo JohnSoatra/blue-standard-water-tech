ee3990f7cde4ba2ee05758699122025ed8b64919
John Soatra <johnsoatra@gmail.com>
2021-10-16 14:12:47 +0700

    good

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ const port = process.env.port || 8080;
 app.use(cors());
 app.get("/*", async (request, response) => {
   const path = request.path.replace("/", "");
+  if (path === "") {
+    response.end(JSON.stringify({msg: "no name"}));
+    return;
+  }
   const file = buck.file(path);
-  const data = await file.get()
-    .then(res => res[0].download()
-      .then(buffer => buffer[0], err => JSON.stringify({msg: "error", content: err}),
-      err => JSON.stringify({msg: "error", content: err}))
-      .catch(err => JSON.stringify({msg: "error", content: err}))
-    .catch(err => JSON.stringify({msg: "error", content: err})));
-  response.end(data);
+  await file.get()
+    .then(res => 
+      res[0].download()
+      .then(buffer =>
+        response.end(buffer[0]), 
+        err => response.end(JSON.stringify({msg: "cannot download", content: err}))
+      )
+      .catch(err =>
+        response.end(JSON.stringify({msg: "error in download", content: err}))
+      ),
+      err => response.end(JSON.stringify({msg: "cannot get", content: err}))
+    )
+    .catch(err => 
+      response.end(JSON.stringify({msg: "error in get", content: err}))
+    );
 });
 
 app.listen(port, () => console.log("App is running!"));
