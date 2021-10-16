d3a6e2f0dcba39707557e40c9fe0939e1da2e65b
John Soatra <johnsoatra@gmail.com>
2021-10-16 12:39:49 +0700

    good

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const firebase = require("./fb_admin");
+const firebase = require("./fb_config/fb_admin");
 const app = express();
-const port = process.env.port || 5000;
+const storage = firebase.storage();
+const buck = storage.bucket("gs://blue-standard.appspot.com");
+const port = process.env.port || 8080;
 
 app.use(cors());
-
 app.get("/*", (req, res) => {
-  res.send({msg: "Home page"});
+  const path = request.path.replace("/", "");
+  await buck.file(path).get().then(success =>
+    success[0].download().then(data =>
+      response.end(data[0])
+    , err => response.json({msg: "error", content: err}))
+  , err =>
+    response.json({msg: "error", content: err})
+  );
 });
 
 app.listen(port, () => console.log("App is running!"));
