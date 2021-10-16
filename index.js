872f7680f03e915df653c840f07035680a2504a3
John Soatra <johnsoatra@gmail.com>
2021-10-16 13:25:55 +0700

    good

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const buck = storage.bucket("gs://blue-standard.appspot.com");
 const port = process.env.port || 8080;
 
 app.use(cors());
-app.get("/*", async (req, response) => {
+app.get("/*", async (request, response) => {
   const path = request.path.replace("/", "");
-  await buck.file(path).get().then(success =>
-    success[0].download().then(data =>
-      response.end(data[0])
-    , err => response.json({msg: "error", content: err}))
-  , err =>
-    response.json({msg: "error", content: err})
-  );
+  const file = buck.file(path);
+  const data = await file.get()
+    .then(res => res[0].download()
+      .then(buffer => buffer[0], err => JSON.stringify({msg: "error", content: err}),
+      err => JSON.stringify({msg: "error", content: err}))
+      .catch(err => JSON.stringify({msg: "error", content: err}))
+    .catch(err => JSON.stringify({msg: "error", content: err})));
+  response.end(data);
 });
 
 app.listen(port, () => console.log("App is running!"));
