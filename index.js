763402b5d842170eb174fb853f615d51e4b6abca
John Soatra <johnsoatra@gmail.com>
2021-10-16 12:44:53 +0700

    ok

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const buck = storage.bucket("gs://blue-standard.appspot.com");
 const port = process.env.port || 8080;
 
 app.use(cors());
-app.get("/*", (req, res) => {
+app.get("/*", async (req, response) => {
   const path = request.path.replace("/", "");
   await buck.file(path).get().then(success =>
     success[0].download().then(data =>
