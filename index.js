3bda53238330afbc1ffd9f9b04dbccd01374597c
John Soatra <johnsoatra@gmail.com>
2021-10-17 11:38:41 +0700

    edit

--- index.js
+++ index.js
@@ -7,29 +7,6 @@ const buck = storage.bucket("gs://blue-standard.appspot.com");
 const port = process.env.port || 8080;
 
 app.use(cors());
-app.get("/test/*", async (request, response) => {
-  const path = request.path.replace("/test/", "");
-  if (path === "") {
-    response.end(JSON.stringify({msg: "no name"}));
-    return;
-  }
-  const file = buck.file(path);
-  await file.get()
-    .then(res => 
-      res[0].download()
-      .then(buffer =>
-        response.end(buffer[0]), 
-        err => response.end(JSON.stringify({msg: "cannot download", content: err}))
-      )
-      .catch(err =>
-        response.end(JSON.stringify({msg: "error in download", content: err}))
-      ),
-      err => response.end(JSON.stringify({msg: "cannot get", content: err}))
-    )
-    .catch(err => 
-      response.end(JSON.stringify({msg: "error in get", content: err}))
-    );
-});
 app.get("/*", async (request, response) => {
   const path = request.path.replace("/", "");
   if (path === "") {
